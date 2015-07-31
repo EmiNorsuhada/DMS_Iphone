91577689608b36f91943c811e6e08d476b66c1db
Emi <emi@infoconnect.com.my>
2015-07-31 11:01:18 +0800

    IndexProfile

--- HelloWorld/IndexViewController.m
+++ HelloWorld/IndexViewController.m
@@ -58,7 +58,7 @@ NSString *colData;
 
 - (IBAction)BackBtn:(id)sender {
 	
-	DMSViewController *controller = [[DMSViewController alloc]init];
+	ProfileViewController *controller = [[ProfileViewController alloc]init];
 	[self presentViewController:controller animated:YES completion:Nil];
 }
 
@@ -72,11 +72,7 @@ NSString *colData;
 }
 
 - (IBAction)TxtDidEnd:(id)sender {
-	
-//	[self parseXMLFileAtURL];
-//	[self.SearchTableView reloadData];
-//	
-//	[UserNameTxt resignFirstResponder];
+
 }
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
@@ -201,9 +197,6 @@ NSString *colData;
 
         [articles addObject:[tempData copy]];
     }
-
-
-		
 }
 
 - (void)parserDidEndDocument:(NSXMLParser *)parser {
@@ -211,6 +204,7 @@ NSString *colData;
 	if (errorParsing == NO)
 	{
 		NSLog(@"%@", articles);
+		[self generateLabel];
 		temp = @"";
 		NSLog(@"XML processing done!");
 	} else {
@@ -218,132 +212,39 @@ NSString *colData;
 	}
 }
 
-#pragma mark - `Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-	return 1;
-}
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-
-	return [articles count];
-}
-
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-	static NSString *CellIdentifier = @"Cell";
+- (void)generateLabel {
 	
-	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-	if (cell == nil) {
-		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
-		cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-	}
-	[[cell.contentView viewWithTag:2001] removeFromSuperview ];
-	[[cell.contentView viewWithTag:2002] removeFromSuperview ];
+//	int totalIdx = [articles.count];
+	NSString *labelName;
 	
+	int addY = 0;
 	if (articles.count != 0) {
-		if(indexPath.row < [articles count]){
+		for (int m = 0; m < articles.count; m = m + 1) {
 			
-			NSString *imgName = [[articles objectAtIndex:indexPath.row ]objectForKey:@"ImageName"];
-			NSArray *split = [imgName componentsSeparatedByString:@"."];
-			NSString *imgType = [split objectAtIndex:[split count] - 1];
+			labelName = [[articles objectAtIndex:m] objectForKey:@"colDesc"];
+			NSLog(@"label name: %@",labelName);
 			
-			//			NSLog(@"tt: %@", imgType);
+			addY = addY + 30;
+//			UILabel *label1=[[UILabel alloc]initWithFrame:CGRectMake(20, 110+addY, 300, 45)];
+//			label1.text = labelName;
+//			label1.font = [UIFont systemFontOfSize:14.0];
+//			label1.backgroundColor =[UIColor clearColor];
+//			[self.view addSubview:label1];
 			
-			UIImageView *imgView = [[UIImageView alloc] initWithFrame:CGRectMake(2, 2, 35, 35)];
-            imgView.image = [UIImage imageNamed:@"doc.png"];
-			
-			[cell.contentView addSubview:imgView];
-			
-			NSString *profileID = [[articles objectAtIndex:indexPath.row ]objectForKey:@"profileID"];
-			NSString *profileName = [[articles objectAtIndex:indexPath.row ]objectForKey:@"profileName"];
-			
-			
-			UILabel *label1=[[UILabel alloc]initWithFrame:CGRectMake(50, -5, 300, 45)];
-			label1.text = profileName;
-			label1.tag = 2001;
-			label1.font = [UIFont systemFontOfSize:14.0];
-			label1.backgroundColor =[UIColor clearColor];
-//			label1.numberOfLines = 2;
-			[cell.contentView addSubview:label1];
-			
-			UILabel *label2=[[UILabel alloc]initWithFrame:CGRectMake(50,7, 300, 45)];
-			label2.text = profileID;
-			label2.tag = 2002;
-			label2.font = [UIFont systemFontOfSize:10.0];
-			label2.textColor = [UIColor grayColor];
-			label2.backgroundColor =[UIColor clearColor];
-			[cell.contentView addSubview:label2];
+			UITextField *textField = [[UITextField alloc] initWithFrame:CGRectMake(20, 110+addY, 280, 25)];
+			textField.borderStyle = UITextBorderStyleRoundedRect;
+			textField.font = [UIFont systemFontOfSize:15];
+			textField.placeholder = labelName;
+			textField.autocorrectionType = UITextAutocorrectionTypeNo;
+			textField.keyboardType = UIKeyboardTypeDefault;
+			textField.returnKeyType = UIReturnKeyDone;
+			textField.clearButtonMode = UITextFieldViewModeWhileEditing;
+			textField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
+			textField.delegate = self;
+			[self.view addSubview:textField];
 		}
 	}
-	
-	[cell setAccessoryType: UITableViewCellAccessoryDetailButton];
-	
-	return cell;
 }
 
-- (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
-	cell.backgroundColor = [UIColor clearColor];
-	cell.backgroundView.backgroundColor = [UIColor clearColor];
-}
-
-#pragma mark - Table view delegate
-
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-     NSLog(@"celltext %@", [[articles objectAtIndex:indexPath.row ]objectForKey:@"DocID"]);
-    
-    NSString *ProfileID = [[articles objectAtIndex:indexPath.row ]objectForKey:@"ProfileID"];
-    NSString *VerID = [[articles objectAtIndex:indexPath.row ]objectForKey:@"VerID"];
-    NSString *post = [NSString stringWithFormat:@"VerID=%@&DocProfileID=%@&FileType=1",VerID,ProfileID];
-    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-    NSString *postLength = [NSString stringWithFormat:@"%d",[postData length]];
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-    [request setURL:[NSURL URLWithString:@"http://192.168.2.28/DocufloSDK/docuflosdk.asmx/ViewFileMobile"]];
-    [request setHTTPMethod:@"POST"];
-    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Content-Type"];
-    [request setHTTPBody:postData];
-    NSURLConnection *conn = [[NSURLConnection alloc] initWithRequest:request delegate:self];
-    NSData *urlData;
-    NSURLResponse *response;
-    NSError *error;
-    urlData = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
-    if(conn) {
-        NSLog(@"Connection Successful");
-    } else {
-        NSLog(@"Connection could not be made");
-    }
-    NSString *aStr = [[NSString alloc] initWithData:urlData encoding:NSUTF8StringEncoding];
-    NSLog(@"astr %@",aStr);
-    NSArray *Separate = [aStr componentsSeparatedByString:@">"];
-    NSString *DEleteAfter = [Separate objectAtIndex:2];
-    NSArray  *LastTrim = [DEleteAfter componentsSeparatedByString:@"</string"];
-    NSString *LAstURL = [LastTrim objectAtIndex:0];
-    NSUserDefaults *pdfURL = [NSUserDefaults standardUserDefaults];
-    NSString *Url =LAstURL;
-    
-    NSString *NameValue = [[articles objectAtIndex:indexPath.row ]objectForKey:@"Name"];
-    NSString *ImageNameValue = [[articles objectAtIndex:indexPath.row ]objectForKey:@"DocType"];
-    NSString * IdNO =[[articles objectAtIndex:indexPath.row ]objectForKey:@"IDNo"];
-    
-    
-    [[NSUserDefaults standardUserDefaults] setObject:NameValue forKey:@"NameValue"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
-    
-    [[NSUserDefaults standardUserDefaults] setObject:ImageNameValue forKey:@"ApplicationNo"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
-    
-    [[NSUserDefaults standardUserDefaults] setObject:IdNO forKey:@"ID"];
-    [[NSUserDefaults standardUserDefaults] synchronize];
-    
-    [pdfURL setObject:Url forKey:@"URL"];
-    pdfView *viewController = [[pdfView alloc] init];
-    [self presentViewController:viewController animated:YES completion:nil];
-    
-}
 
 @end
